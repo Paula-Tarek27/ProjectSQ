b1d9d8851e14672bfe8c509db58e2a47ee4d9ff5
ahmed Desoky <84643423+ADesoky@users.noreply.github.com>
2023-05-20 22:18:49 +0300

    last error
    na zwdt al paypal w login wl regester bs aldnia baiza errors  w css mn n7it login wlregister

---
+++ login.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once("includes/config.php");
+require_once("includes/classes/FormSanitizer.php");
+require_once("includes/classes/Account.php");
+require_once("includes/classes/Constants.php");
+
+$account = new Account($con);
+
+if (isset($_POST["submitButton"])) {
+
+    $userName = FormSanitizer::sanitizeFormUsername($_POST["userName"]);
+    $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+
+    $success = $account->login($userName, $password);
+
+    if ($success) {
+        $_SESSION["userLoggedIn"] = $userName;
+
+        header("Location: index.php");
+    } else {
+        echo '<script>alert("Failed")</script>';
+    }
+}
+
+
+function getInputValue($name)
+{
+    if (isset($_POST[$name])) {
+        echo $_POST[$name];
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>welcome to NETFLIX</title>
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+</head>
+
+<body>
+    <div class="signIn">
+        <div class="column">
+            <div class="header">
+                <img src="assets/images/logo.png" alt="Site logo" title="Logo">
+                <h3>Sign In</h3>
+                <span>to continue to NETFLIX</span>
+
+            </div>
+
+            <form action="" method="POST">
+                <?php echo $account->getError(Constants::$loginFailed); ?>
+                <input type="text" name="userName" placeholder="UserName" value="<?php getInputValue("userName") ?>" required>
+
+                <input type="password" name="password" placeholder="Password" required>
+
+                <input type="submit" name="submitButton" value="SUBMIT">
+            </form>
+
+            <a href="register.php" class="signInMessage">Need an Account ? Sign Up here!</a>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
